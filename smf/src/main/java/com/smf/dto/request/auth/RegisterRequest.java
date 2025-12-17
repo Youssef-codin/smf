5fb95ce8d86ecc97502ebb6ce95f6cd661a89dff
Your Name <youremail@example.com>
2025-12-17 21:11:30 +0200

    Clean up old smf folder and update Device.java

--- smf/src/main/java/com/smf/dto/request/auth/RegisterRequest.java
+++
@@ -1,8 +0,0 @@
-package com.smf.dto.request.auth;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
-public record RegisterRequest(@NotBlank @Email String email, @NotBlank String username,
-		@NotBlank String password) {
-}
--- smf/src/main/java/com/smf/dto/response/api/JwtResponse.java
+++
@@ -1,6 +0,0 @@
-package com.smf.dto.response.api;
-
-import java.util.UUID;
-
-public record JwtResponse(UUID id, String Token) {
-}
