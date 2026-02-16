fad4b9fc742b69566d5780f6f4354e5eafea45e9
Youssef-codin <youssef240100538@sut.edu.eg>
2026-02-17 00:25:23 +0200

    batman
    Tests done for zone

--- src/main/java/com/smf/dto/zone/ZoneEntryRequest.java
+++ src/main/java/com/smf/dto/zone/ZoneEntryRequest.java
@@ -2,5 +2,5 @@ package com.smf.dto.zone;
 
 import java.util.UUID;
 
-//NOTE: zoneId is temporary - will be replaced with coordinate-based zone detection
+// NOTE: zoneId is temporary, will be replaced with coordinate-based zone detection
 public record ZoneEntryRequest(UUID zoneId) {}
