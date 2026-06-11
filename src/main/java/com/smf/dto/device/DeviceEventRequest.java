3cba320bb89274cd4a8f29be23a59e48327da171
Youssef-codin <youssef240100538@sut.edu.eg>
2026-06-11 13:23:54 +0300

    bug: added back metadata

--- src/main/java/com/smf/dto/device/DeviceEventRequest.java
+++ src/main/java/com/smf/dto/device/DeviceEventRequest.java
@@ -3,6 +3,10 @@ package com.smf.dto.device;
 import com.smf.model.enums.EventTypes;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.util.Map;
 
-public record DeviceEventRequest(@NotBlank String macAddress, @NotNull EventTypes event) {}
+public record DeviceEventRequest(
+    @NotBlank String macAddress,
+    @NotNull EventTypes event,
+    Map<String, Object> metadata) {}
 
